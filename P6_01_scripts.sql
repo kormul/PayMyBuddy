eaa11b1b5a7ce9705f617bf0f3d92b7350986d27
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2022-01-21 23:49:13 +0100

    update script sql

--- P6_01_scripts.sql
+++ P6_01_scripts.sql
@@ -8,39 +8,43 @@ USE `paymybuddy`;
 DROP TABLE IF EXISTS `connections`;
 CREATE TABLE `connections` (
   `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `connectionid` int(11) NOT NULL
+  `connection_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL,
-  `amount` double NOT NULL,
-  `description` text NOT NULL,
-  `transmitter` int(11) NOT NULL,
-  `receiver` int(11) NOT NULL,
-  `fee` double NOT NULL
+  `amount` double DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `fee` double DEFAULT NULL,
+  `receiver` int(11) DEFAULT NULL,
+  `transmitter` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `mail` text NOT NULL,
-  `pseudo` text NOT NULL,
-  `password` text NOT NULL,
-  `currency` double NOT NULL
+  `currency` double DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `pseudo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 ALTER TABLE `connections`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK62y8qblm7a5kvbeigy3lm08lp` (`connection_id`),
+  ADD KEY `FKltpo1ymtaafd67hx5tls1db6u` (`user_id`);
 
 ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK9o43uaqw7vwkwhukuesd0t336` (`receiver`),
+  ADD KEY `FKqep1wbn39twjnghxtbyt5hw4r` (`transmitter`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
-
 
 ALTER TABLE `connections`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
@@ -50,4 +54,12 @@ ALTER TABLE `transactions`
 
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `connections`
+  ADD CONSTRAINT `FK62y8qblm7a5kvbeigy3lm08lp` FOREIGN KEY (`connection_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `FKltpo1ymtaafd67hx5tls1db6u` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `FK9o43uaqw7vwkwhukuesd0t336` FOREIGN KEY (`receiver`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `FKqep1wbn39twjnghxtbyt5hw4r` FOREIGN KEY (`transmitter`) REFERENCES `users` (`id`);
 COMMIT;
